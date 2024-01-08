45043f25ae8c8275e12309291246b1e305899374
BlueNova <belal.meshaal123@gmail.com>
2024-01-08 10:47:41 +0100

    Add HomeComponent import

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [HomeComponent, AppComponent],
   imports: [
