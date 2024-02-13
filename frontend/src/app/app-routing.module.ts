ec72157cc89fa912cd9d260af13e146003aa6eca
Belal Meshaal <bmeshaal@united-internet.de>
2024-02-13 11:19:32 +0100

    Refactor routing and component imports

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ListComponent } from './list/list.component';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 
 @NgModule({
-  declarations: [HomeComponent, AppComponent, ListComponent],
   imports: [
     RouterModule.forRoot(routes),
     CommonModule,
     FormsModule
   ],
-  bootstrap: [AppComponent]
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ListComponent } from './list/list.component';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'list', component: ListComponent },
     { path: '', redirectTo: '' }
 ];
